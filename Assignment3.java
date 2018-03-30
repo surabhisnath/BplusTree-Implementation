aa3bf83221f460015eb92462135c83161d1b9a28
surabhisnath <surabhi16271@iiitd.ac.in>
2018-03-30 21:08:56 +0530

    Working wrong

--- Assignment3.java
+++ Assignment3.java
@@ -58,12 +58,70 @@ class findret
 }
 
 
+
+
+class OrderRecordsID implements Comparator<record>
+{
+	//int mode;
+	@Override
+	public int compare(record one, record two)
+	{
+		if(one.instructor_id.compareTo(two.instructor_id) > 0)
+				return 1;
+		else
+				return -1;
+	}
+}
+
+
+class OrderRecordsName implements Comparator<record>
+{
+	@Override
+	public int compare(record one, record two)
+	{
+		if(one.instructor_name.compareTo(two.instructor_name) > 0)
+			return 1;
+		else
+			return -1;
+	}
+}
+
+class OrderRecordsDept implements Comparator<record>
+{
+	@Override
+	public int compare(record one, record two)
+	{
+		if(one.department.compareTo(two.department) > 0)
+			return 1;
+		else
+			return -1;
+	}
+}
+
+
+class OrderRecordsSal implements Comparator<record>
+{
+	@Override
+	public int compare(record one, record two)
+	{
+		if(one.salary > two.salary)
+			return 1;
+		else
+			return -1;
+	}
+}
+
+
+
+
+
+
 class Btree
 {
 	int num_nodes = 0;
 	ArrayList<record> db;
 	ArrayList<record> shallowdb;
-	int N = 10;
+	int N = 2;
 	
 	
 	treenode root;
@@ -134,6 +192,7 @@ class Btree
 			{
 				if(compare(r.keys.get(j), v) > 0)
 				{
+					
 					i = j;
 					counter=1;
 					break;
@@ -173,6 +232,7 @@ class Btree
 		
 		for(int l = 0; l<r.keys.size(); l++)
 		{
+			System.out.println(r.keys.get(l));
 			if(v.equals(r.keys.get(l)))
 			{
 				System.out.println(v);
@@ -529,7 +589,7 @@ class Btree
 		{
 			System.out.println("D");
 			treenode r = findhelp(value);
-					
+			
 			if(r.keys.size()<(N-1))
 			{
 				System.out.println("E");
@@ -608,7 +668,7 @@ class Btree
 				{
 					r.keys.add(h, t.keys.get(h));
 					r.pointers_r.add(h,new ArrayList<record>());
-					r.pointers_r.set(h, t.pointers_r.get(h));
+					r.pointers_r.add(h, t.pointers_r.get(h));
 				}
 				
 				for(int m=(int)Math.ceil(N/2); m<N; m++)     
@@ -642,6 +702,7 @@ class Btree
 		}
 		
 		treenode p = a.parent;
+		
 		if(p.pointers_tn.size()<N)
 		{
 			System.out.println("N");
@@ -651,6 +712,7 @@ class Btree
 			{
 				
 				if(p.pointers_tn.get(i)==a)
+		
 				{
 					index=i;
 					break;
@@ -669,11 +731,13 @@ class Btree
 			System.out.println("P");
 			treenode t = new treenode();
 			
-			for(int i=0;i<N;i++)
+			for(int i=0;i<N-1;i++)
 			{
 				t.keys.add(p.keys.get(i));
 				t.pointers_tn.add(p.pointers_tn.get(i));
 			}
+			
+			t.pointers_tn.add(p.pointers_tn.get(N-1));
 			
 			
 			int index = 0;
@@ -682,13 +746,14 @@ class Btree
 				if(p.pointers_tn.get(i)==a)
 				{
 					index=i;
+				
 					break;
 				}
 			}
 			
 			System.out.println("Q");
 			t.pointers_tn.add(index+1,c);
-			t.keys.add(index+1,b);
+			t.keys.add(index,b);
 			
 			p.keys = null;
 			p.keys = new ArrayList<String>();
@@ -856,361 +921,35 @@ public class Assignment3
 //		
 //		System.out.println(child.parent.pointers_tn.get(0)==child);
 		
-		record first = new record("1111","A","A","A",1234);
-		record second = new record("1111","B","B","B",1234);
-		record third = new record("1111","C","A","A",1234);
-		record fourth = new record("1111","D","B","B",1234);
-		record fifth= new record("1111","E","A","A",1234);
-        record six = new record("1111","F","B","B",1234);
-		record seven = new record("1111","Gb","A","A",1234);
-		record eight = new record("1111","Ga","B","B",1234);
-		record nine = new record("1111","G","B","B",1234);
-		record eleven = new record("1111","H","B","B",1234);
-		record ten = new record("1111","Freeq","B","B",1234);
-		record twel = new record("1111","I","B","B",1234);
-		record thirt = new record("1111","J","B","B",1234);
-		record fourt = new record("1111","Da","B","B",1234);
-		record fift =  new record("1111","Db","B","B",1234);
-		record sixt = new record("1111","Ca","B","B",1234);
-		record sevent =  new record("1111","Cb","B","B",1234);
-		record eightt = new record("1111","Ia","B","B",1234);
-		record ninet =  new record("1111","Ib","B","B",1234);
-		record thirty = new record("1111","Ayne","B","B",1234);
-		record thirto = new record("1111","Ona","B","B",1234);
+		record one = new record("1","oneID","oneName","oneDept",1.0);
+		record two = new record("1","twoID","twoName","twoDept",2.0);
+		record three = new record("1","threeID","threeName","threeDept",3.0);
+		record four = new record("1","fourID","fourName","fourDept",4.0);
+		record five = new record("1","fiveID","fiveName","fiveDept",5.0);
 		
-		record sev = new record("1111","You","B","B",1234);
-		record a = new record("1111","Jone","B","B",1234);
-		record b = new record("1111","Oye","B","B",1234);
-		record c = new record("1111","TOna","B","B",1234);
-		record d = new record("1111","Tni","B","B",1234);
-		record e = new record("1111","Yiu","B","B",1234);
-		record f = new record("1111","Zoi","B","B",1234);
-		record g = new record("1111","Oyi","B","B",1234);
-		record h = new record("1111","Onab","B","B",1234);
-		record p = new record("1111","Tani","B","B",1234);
-		record y = new record("1111","Bob","B","B",1234);
-		record z = new record("1111","Cat","B","B",1234);
-		record k = new record("1111","Dark","B","B",1234);
-		record l = new record("1111","Abiee","B","B",1234);
-		record q = new record("1111","Cani","B","B",1234);
-		record r = new record("1111","Dob","B","B",1234);
-		record s = new record("1111","Dat","B","B",1234);
-		record t = new record("1111","Cark","B","B",1234);
-		record u = new record("1111","Dbiee","B","B",1234);
-		record A = new record("1111","Aone","B","B",1234);
-		record B = new record("1111","Aye","B","B",1234);
-		record C = new record("1111","AOna","B","B",1234);
-		record D = new record("1111","Ani","B","B",1234);
-		record E = new record("1111","Aiu","B","B",1234);
-		record F = new record("1111","Eoi","B","B",1234);
-		record G = new record("1111","Eyi","B","B",1234);
-		record H = new record("1111","Enab","B","B",1234);
-		record P = new record("1111","Eani","B","B",1234);
-		record Y = new record("1111","Eob","B","B",1234);
-		record Z = new record("1111","Yat","B","B",1234);
-		record K = new record("1111","Yark","B","B",1234);
-		record L = new record("1111","YAbiee","B","B",1234);
-		record Q = new record("1111","Yani","B","B",1234);
-		record R = new record("1111","Ydob","B","B",1234);
-		record S = new record("1111","Zat","B","B",1234);
-		record T = new record("1111","Zark","B","B",1234);
-		record U = new record("1111","Zbiee","B","B",1234);
-		record W = new record("1111","Zani","B","B",1234);
-		record N = new record("1111","Zdob","B","B",1234);
-		record twe = new record("1111","TZani","B","B",1234);
-		record tweo = new record("1111","Tdob","B","B",1234);
-		record first1 = new record("1111","Tanya","A","A",1234);
-		record second1 = new record("1111","Aarushi","B","B",1234);
-		record third1 = new record("1111","Aishwarya","A","A",1234);
-		record fourth1 = new record("1111","4","B","B",1234);
-		record fifth1= new record("1111","5","A","A",1234);
-		record six1 = new record("1111","6","6","B",1234);
-		record seven1 = new record("1111","7","A","A",1234);
-		record eight1 = new record("1111","8","B","B",1234);
-		record nine1 = new record("1111","9","B","B",1234);
-		record eleven1 = new record("1111","10","B","B",1234);
-		record ten1 = new record("1111","11","B","B",1234);
-		record twel1 = new record("1111","I3","B","B",1234);
-		record thirt1 = new record("1111","J5","B","B",1234);
-		record fourt1 = new record("1111","K5","B","B",1234);
-		record fift1 = new record("1111","Ay4ne","B","B",1234);
-		record sixt1 = new record("1111","Onka","B","B",1234);
-		record sev1 = new record("1111","Yoklu","B","B",1234);
-		record a1 = new record("1111","Jonme","B","B",1234);
-		record b1 = new record("1111","Oymke","B","B",1234);
-		record c1 = new record("1111","TgkOna","B","B",1234);
-		record d1 = new record("1111","Tnji","B","B",1234);
-		record e1 = new record("1111","Yijgu","B","B",1234);
-		record f1 = new record("1111","Zjhoi","B","B",1234);
-		record g1 = new record("1111","jfOyi","B","B",1234);
-		record h1 = new record("1111","Onhfab","B","B",1234);
-		record p1 = new record("1111","Thkani","B","B",1234);
-		record y1 = new record("1111","Bklhob","B","B",1234);
-		record z1 = new record("1111","Cklat","B","B",1234);
-		record k1 = new record("1111","Dkrgark","B","B",1234);
-		record l1 = new record("1111","Akfhlbiee","B","B",1234);
-		record q1 = new record("1111","Ckfhani","B","B",1234);
-		record r1 = new record("1111","Dhkfob","B","B",1234);
-		record s1 = new record("1111","Dkgat","B","B",1234);
-		record t1 = new record("1111","Cklfark","B","B",1234);
-		record u1 = new record("1111","Dlfbiee","B","B",1234);
-		record A1 = new record("1111","Alfone","B","B",1234);
-		record B1 = new record("1111","Alfye","B","B",1234);
-		record C1 = new record("1111","AlOna","B","B",1234);
-		record D1 = new record("1111","Alfni","B","B",1234);
-		record E1 = new record("1111","Ahkiu","B","B",1234);
-		record F1 = new record("1111","gEoi","B","B",1234);
-		record G1 = new record("1111","Eygi","B","B",1234);
-		record H1 = new record("1111","Enfjab","B","B",1234);
-		record P1 = new record("1111","Ekggfani","B","B",1234);
-		record Y1 = new record("1111","Egfob","B","B",1234);
-		record Z1 = new record("1111","Ygkat","B","B",1234);
-		record K1 = new record("1111","Ylfark","B","B",1234);
-		record L1 = new record("1111","YgfAbiee","B","B",1234);
-		record Q1 = new record("1111","Ykgani","B","B",1234);
-		record R1 = new record("1111","Yjgfdob","B","B",1234);
-		record S1 = new record("1111","Zklffgat","B","B",1234);
-		record T1 = new record("1111","Zagkfflkrk","B","B",1234);
-		record U1 = new record("1111","Zkfkgfbiee","B","B",1234);
-		record W1 = new record("1111","Zgkani","B","B",1234);
-		record N1 = new record("1111","Zgfjdob","B","B",1234);
-		record twe1 = new record("1111","Tgfani","B","B",1234);
-		record tweo1 = new record("1111","Tgfdob","B","B",1234);
+		tree.insert(one);
+		tree.insert(two);
+		tree.insert(three);
+		tree.insert(four);
+		tree.insert(five);
 		
+		findret leaf1 = tree.find("1.0");
+		System.out.println(leaf1==null);
+		System.out.println();
+		treenode leafone =leaf1.r;
 		
+		while(leafone!=null)
+		{
+			for(int i=0;i<leafone.keys.size();i++)
+			{
+				System.out.println(leafone.keys.get(i));
+			}
+			
+			leafone = leafone.right_leaf;
+			
+		}
 		
-		tree.insert(first);
-		tree.insert(second);
-		tree.insert(third);
-		tree.insert(fourth);
-		tree.insert(fifth);
-		tree.insert(six);
-		tree.insert(nine);
-		tree.insert(eleven);
-		tree.insert(twel);
-		tree.insert(thirt);
-		tree.insert(fourt);
-		tree.insert(fift);
-		tree.insert(sixt);
-		tree.insert(sev);
-		tree.insert(a);
-		tree.insert(b);
-		tree.insert(c);
-		tree.insert(d);
-		tree.insert(e);
-		tree.insert(f);
-		tree.insert(g);
-		tree.insert(h);
-		tree.insert(p);
-		tree.insert(y);
-		tree.insert(z);
-		tree.insert(k);
-		tree.insert(l);
-		tree.insert(q);
-		tree.insert(fift);
-		tree.insert(sixt);
-		tree.insert(sevent);
-		tree.insert(seven);
-		tree.insert(eight);
-		tree.insert(ninet);
-		tree.insert(eightt);
-		tree.insert(r);
-		tree.insert(s);
-		tree.insert(t);
-		tree.insert(u);
-		tree.insert(a);
-		tree.insert(B);
-		tree.insert(C);
-		tree.insert(D);
-		tree.insert(E);
-		tree.insert(F);
-		tree.insert(G);
-		tree.insert(H);
-		tree.insert(P);
-		tree.insert(Y);
-		tree.insert(Z);
-		tree.insert(K);
-		tree.insert(L);
-		tree.insert(Q);
-		tree.insert(R);
-		tree.insert(S);
-		tree.insert(T);
-		tree.insert(U);
-		tree.insert(W);
-		tree.insert(N);
-		tree.insert(twe);
-		tree.insert(tweo);
-		tree.insert(first);
-		tree.insert(second);
-		tree.insert(third);
-		tree.insert(fourth);
-		tree.insert(fifth);
-		tree.insert(six);
-		tree.insert(seven);
-		tree.insert(eight);
-		tree.insert(nine);
-		tree.insert(eleven);
-		tree.insert(twel);
-		tree.insert(thirt);
-		tree.insert(fourt);
-		tree.insert(fift);
-		tree.insert(sixt);
-		tree.insert(sev);
-		tree.insert(a);
-		tree.insert(b);
-		tree.insert(c);
-		tree.insert(d);
-		tree.insert(e);
-		tree.insert(f);
-		tree.insert(g);
-		tree.insert(h);
-		tree.insert(p);
-		tree.insert(y);
-		tree.insert(z);
-		tree.insert(k);
-		tree.insert(l);
-		tree.insert(q);
-		tree.insert(r);
-		tree.insert(s);
-		tree.insert(t);
-		tree.insert(u);
-		tree.insert(a);
-		tree.insert(B);
-		tree.insert(C);
-		tree.insert(D);
-		tree.insert(E);
-		tree.insert(F);
-		tree.insert(G);
-		tree.insert(H);
-		tree.insert(P);
-		tree.insert(Y);
-		tree.insert(Z);
-		tree.insert(K);
-		tree.insert(L);
-		tree.insert(Q);
-		tree.insert(R);
-		tree.insert(S);
-		tree.insert(T);
-		tree.insert(U);
-		tree.insert(W);
-		tree.insert(N);
-		tree.insert(twe);
-		tree.insert(tweo);
-		tree.insert(first1);
-		tree.insert(second1);
-		tree.insert(third1);
-		tree.insert(fourth1);
-		tree.insert(fifth1);
-		tree.insert(six1);
-		tree.insert(seven1);
-		tree.insert(eight1);
-		tree.insert(ten1);
-		tree.insert(nine1);
-		tree.insert(eleven1);
-		tree.insert(twel1);
-		tree.insert(thirt1);
-		tree.insert(fourt1);
-		tree.insert(fift1);
-		tree.insert(sixt1);
-		tree.insert(sev1);
-		tree.insert(a1);
-		tree.insert(b1);
-		tree.insert(c1);
-		tree.insert(d1);
-		tree.insert(e1);
-		tree.insert(f1);
-		tree.insert(g1);
-		tree.insert(h1);
-		tree.insert(p1);
-		tree.insert(y1);
-		tree.insert(z1);
-		tree.insert(k1);
-		tree.insert(l1);
-		tree.insert(q1);
-		tree.insert(r1);
-		tree.insert(s1);
-		tree.insert(t1);
-		tree.insert(u1);
-		tree.insert(a1);
-		tree.insert(B1);
-		tree.insert(C1);
-		tree.insert(D1);
-		tree.insert(E1);
-		tree.insert(F1);
-		tree.insert(G1);
-		tree.insert(H1);
-		tree.insert(P1);
-		tree.insert(Y1);
-		tree.insert(Z1);
-		tree.insert(K1);
-		tree.insert(L1);
-		tree.insert(Q1);
-		tree.insert(R1);
-		tree.insert(S1);
-		tree.insert(T1);
-		tree.insert(U1);
-		tree.insert(W1);
-		tree.insert(N1);
-		tree.insert(twe1);
-		tree.insert(tweo1);
-		tree.insert(ten);
-		tree.insert(thirty);
-		tree.insert(thirto);
 		
  	}
 
 }
-
-
-class OrderRecordsID implements Comparator<record>
-{
-	//int mode;
-	@Override
-	public int compare(record one, record two)
-	{
-		if(one.instructor_id.compareTo(two.instructor_id) > 0)
-				return 1;
-		else
-				return -1;
-	}
-}
-
-
-class OrderRecordsName implements Comparator<record>
-{
-	@Override
-	public int compare(record one, record two)
-	{
-		if(one.instructor_name.compareTo(two.instructor_name) > 0)
-			return 1;
-		else
-			return -1;
-	}
-}
-
-class OrderRecordsDept implements Comparator<record>
-{
-	@Override
-	public int compare(record one, record two)
-	{
-		if(one.department.compareTo(two.department) > 0)
-			return 1;
-		else
-			return -1;
-	}
-}
-
-
-class OrderRecordsSal implements Comparator<record>
-{
-	@Override
-	public int compare(record one, record two)
-	{
-		if(one.salary > two.salary)
-			return 1;
-		else
-			return -1;
-	}
-}
-
-
